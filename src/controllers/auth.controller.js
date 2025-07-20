25bc1468ef8975db2f95b21d7db61b00d5fe4b52
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-07-19 22:16:27 -0500

    feat(users): add user creation route and controller with DB persistence.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import Users from '../models/users.model.js';
+
 // Vista para mostrar el formulario de registro
 const formRegister = (req, res) =>{
     res.render('auth/register', {
@@ -6,6 +8,31 @@ const formRegister = (req, res) =>{
     });
 }
 
+const register = async (req, res) =>{
+    // Extraer los datos del formulario
+    const {name, email, password} = req.body;
+
+    // Almacenar el usuario
+    const user  = await Users.create({
+        name,
+        email,
+        password
+    });
+
+    const newUser = await user.save();
+
+    // Si no se crea el usuario
+    if(!newUser){
+        return next();
+    }
+
+    // Redireccionar la usuario
+    res.redirect('/login');
+
+
+}
+
 export{
-    formRegister
+    formRegister,
+    register
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import express from  'express';
-import {formRegister} from '../controllers/auth.controller.js';
+import {formRegister, register} from '../controllers/auth.controller.js';
 const router = express.Router();
 
 
 // Router GET
 router.get('/register', formRegister);
+
+//Router POST
+router.post('/register', register);
 
 export default router
