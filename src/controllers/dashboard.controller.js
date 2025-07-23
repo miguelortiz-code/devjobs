d47ec45348a53b25bdba8bb89efb8898eb218b01
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-07-23 17:26:56 -0500

    feat(dashboard): display vacancies created by the logged-in user.

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,7 +1,15 @@
-const viewAdmin = (req, res) =>{
+import {Vacancy} from '../models/index.model.js';
+
+const viewAdmin = async (req, res) =>{
+    // Extraer el id del autor de la vacante
+    const {_id} = req.user;
+    // consultar en la bd el id del usuario
+    const  vacancy = await Vacancy.find({autor: _id})
+    // Renderizar el panel administrativo
     res.render('admin/dashboard',{
         namePage: 'Panel administativo',
-        tagline: 'Crea y administra tus vacantes desde aquí'
+        tagline: 'Crea y administra tus vacantes desde aquí',
+        vacancy
     })
 }
 
