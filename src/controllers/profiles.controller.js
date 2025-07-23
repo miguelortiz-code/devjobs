56df6fb9fd9acaa9dcc9d8e0327e615ecab2d014
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-07-23 18:38:20 -0500

    feat(users): add edit profile view with dynamic data and style input name field.

--- src/controllers/profiles.controller.js
+++ src/controllers/profiles.controller.js
@@ -1,5 +1,24 @@
-const viewProfile = (req, res ) =>{
-    res.send('Información y formulario de perfil');
+import {Users} from '../models/index.model.js';
+
+const viewProfile = async (req, res ) =>{
+    try {
+        // Extraer el id del usuario que inicio sesión
+        const {_id} = req.user;
+        // Consultar en la base de datos el usuario
+        const user = await Users.findById(_id);
+        // Si no hay usuario autenticado
+        if (!user) {
+            return  res.redirect('/auth/login');
+        }
+
+        res.render('auth/profile',{
+            namePage: 'Edita tu perfil en devJobs',
+            user
+        });   
+    } catch (error) {
+        console.log(`Error al obtener la información del usuario: ${error}`);
+        res.redirect('/auth/login');
+    }
 }
 
 export{
