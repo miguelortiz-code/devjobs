ffbe41b29cfd29da9aef4f3c4252fe0eaaa73ed0
Miguel Ortiz <miguelortiz.code@gmail.com>
2025-08-10 01:35:43 -0500

    feat(auth): configure nodemailer with handlebars templates for password reset emails.

---
+++ src/helpers/email.helper.js
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer';
+import hbs from 'nodemailer-express-handlebars';
+import util from 'util';
+
+let transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth:{
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+})
